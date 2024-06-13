4057b75cfa2e0085def885dcac8f8d8152b88119
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-13 15:12:22 +0530

    sss

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route path='/' element={<Login/>}/>
+    <Route path='/Register' element={<Register/>}/>
    </Routes>
    </BrowserRouter>
   );
